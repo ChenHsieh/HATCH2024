c49ff0167c1263abaf9ba8f1ade1f4f35ae46db4
chen <chen.hsieh@uga.edu>
2024-03-03 12:24:06 -0600

    refine the AI part

--- README.md
+++ README.md
@@ -1,7 +1,51 @@
-# HATCH2024
+# HATCH2024 - Nutrigenomics One Stop Shop by Team IOB UGA
 
-## Nutrigenomics
+## Team Members
 
-### Team Members
+We are a group of PhD students from the Institute of Bioinformatics at University of Georgia. Teams members are:
 
+Chen Hsieh
+Darrian Royce Talamentes
+Mary-Frances Behnke
 
+## Project Description
+
+We are building a suite for commanders who is in charge of making sure that the crews are well-fed and healthy. The suite will be able to take in the crews' genetic information and provide a list of food that the soldiers should eat to maintain a healthy lifestyle and also optimize the payload.
+
+## Project Goals
+
+1. IDENTIFY the nutrigenomic profile from the given individuals.​
+
+* Retrieve annotations for given genes from external databases.​
+
+* SNP calling – identify variants and polymorphisms that result in unique nutritional needs (might require lit review, but also think about how we can do this bioinformatically)​
+
+2. MAXIMIZE nutrition.​
+
+* Here’s where we want to retrieve data from FooDB. I think it would be best to scrape this data directly from the web to reduce our app size, but we can also subset the full database first (e.g. only include plant-based foods) as another way to reduce the storage needed to by our app.​
+
+* Keep in mind that the challenge wants it to be vegetable-based (we need to ask if that includes plant-based foods like tofu?), but there may be some flexibility here depending on which genes were provided in the fastas, which might require some quick literature review! (i.e., did the genes have to do with how the individual metabolizes macros like proteins and carbohydrates, or other nutrients like vitamins and minerals?)​
+
+3. MINIMIZE payload weight.​
+
+* Dehydration is a great way to reduce cargo volume and weight (FooDB includes water weight so we can do some calculations there)​
+
+* However, Vitamins A and C are compromised/destroyed during dehydration process so they will need special consideration. I am not suggesting we devise a way to send hydrated food to space (I checked, this is not super feasible) – a better solution would be if the report generated a “flag” or “warning” for Vitamins A and C that recommends non-food supplements to fulfill those nutrient requirements.​
+
+4. REPORT optimal vegetables/nutritional plans (and possibly alternates).​
+
+* One way to quantitatively identify and report optimal vegetables for the space mission is to make a table based on nutritional value, which is directly populated from FooDB. From there we can develop some simple system that scores all vegetables in the database according to nutritional value per unit dry weight and scales the importance of a given nutrient based on the nutritional needs of the input group (see next slide).​
+
+* Then, the nutritionally optimal fruits/vegetables are plotted on an x-y axis, where one axis is dehydrated weight, and the other axis is “nutritional value.”​
+
+## Getting started
+
+There are 3 main components to make use of the deliverables. The first is the SnakeMake pipeline we have for reproducible, scalable, and efficient data processing to do the variant calling. The Sencond is the cleaned database from FOODB for optimized recommendation. The third is the dashboard that we have built using Streamlit for the commander to research on genes and do data visualization. 
+
+please replace the conda env as you see fit, we provided a sample conda env `data/envionments.yml` for you to use but it is not tested yet.
+
+```bash
+cd dashboard
+conda activate ~/opt/anaconda3/envs/streamlit
+streamlit run app.py
+```
--- dashboard/app.py
+++ dashboard/app.py
@@ -4,44 +4,15 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from Bio import Entrez
+# from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
+import time
+import streamlit.components.v1 as components
+
+
 
 ## helper function to search pubmed
-@st.cache
-def search_pubmed(keyword):
-    # Your email here
-    Entrez.email = "hatch2024@gmail.com"
-
-    # Define the search term ("MTHFR" in this case) and the database
-    search_term = keyword
-    database = "pubmed"
-
-    # Use Entrez.esearch to search for the keyword in the database
-    search_handle = Entrez.esearch(db=database, term=search_term, retmax=10)
-    search_results = Entrez.read(search_handle)
-    search_handle.close()
-
-    # Extract the list of Ids for the found articles
-    id_list = search_results["IdList"]
-    
-    # Fetch details of the articles using Entrez.efetch
-    fetch_handle = Entrez.efetch(db=database, id=",".join(id_list), retmode="xml")
-    articles = Entrez.read(fetch_handle)
-    fetch_handle.close()
-
-    # Process and print out some information about each article
-    for article in articles['PubmedArticle']:
-        # Extract basic information
-        article_title = article['MedlineCitation']['Article']['ArticleTitle']
-        authors_list = article['MedlineCitation']['Article']['AuthorList']
-        authors = ', '.join([auth['LastName'] + " " + auth.get('Initials','') for auth in authors_list if 'LastName' in auth])
-        pub_date = article['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']
-        year = pub_date.get('Year', 'Unknown year')
-
-        print(f"Title: {article_title}")
-        print(f"Authors: {authors}")
-        print(f"Publication Year: {year}")
-        print("-" * 100)
-# Function to search PubMed
+@st.cache_data
 def search_pubmed(keyword):
     Entrez.email = "your_email@example.com"  # Use a valid email
     try:
@@ -89,52 +60,93 @@ def get_article_info(article, article_id):
 
 def stream_df(article_df):
     yield article_df
+
+def stream_data(text):
+    for word in text.split():
+        yield word + " "
+        time.sleep(0.02)
+
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    output = llm(input_text)
+    # st.info(llm(input_text))
+    with st.chat_message("user"):
+        # st.write("Hello Commander 👋")
+        st.write_stream(stream_data(output))
+
+
 # web app structure
 st.title("Nutrigenomics One Stop Shop")
 
 st.write("Welcome to the Nutrigenomics Dashboard! This is a demo of the dashboard for the HATCH2024 project by IOB UGA team.")
 
-st.header("Genes of interest")
+st.header("BiteSizedGenes")
+st.write("To search for articles related to a gene of interest and summarize them using OpenAI's GPT-3.5 Turbo model.")
+
+openai_api_key = st.text_input('Please input your own OpenAI API Key', type='password')
 
 # Search box
 keyword = st.text_input('Enter a keyword to search for articles:', '')
+show_prompt = st.toggle('show prompt for OpenAI summarization', False)
 
 if st.button("Use MTHFR as example input"):
     keyword = "MTHFR"
 
 if keyword:
-    ids = search_pubmed(keyword)  # Assuming search_pubmed function is defined elsewhere
-    if ids:
-        st.write("You can click on the article titles to view more details.")
-        st.write(f"Showing the top {len(ids)} articles")
-        
-        articles_info = []
-        for article_id in ids:
-            details = fetch_article_details(article_id)
-            if details:
-                article = details["PubmedArticle"][0]
-                title, title_link, authors, year, abstract = get_article_info(article, article_id)
-                articles_info.append([title, title_link, authors, year, abstract])
-        
-        # Create a DataFrame
-        article_df = pd.DataFrame(articles_info, columns=['Title', 'link', 'Authors', 'Year', 'Abstract'])
-        article_df.set_index("Year", inplace=True)
+    with st.spinner(f"Searching for articles related to {keyword}..."):
+    # st.write(f"Searching for articles related to {keyword}...")
+        ids = search_pubmed(keyword)  # Assuming search_pubmed function is defined elsewhere
+        if ids:
+            st.write("You can click on the article titles to view more details.")
+            st.write(f"Showing the top {len(ids)} articles")
+            
+            articles_info = []
+            for article_id in ids:
+                details = fetch_article_details(article_id)
+                if details:
+                    article = details["PubmedArticle"][0]
+                    title, title_link, authors, year, abstract = get_article_info(article, article_id)
+                    articles_info.append([title, title_link, authors, year, abstract])
+            
+            # Create a DataFrame
+            article_df = pd.DataFrame(articles_info, columns=['Title', 'link', 'Authors', 'Year', 'Abstract'])
+            article_df.set_index("Year", inplace=True)
 
-        
-        st.write(article_df)
+            
+            st.write(article_df)
 
-        st.write(f"For more search result please visit: https://www.ncbi.nlm.nih.gov/search/all/?term={keyword}")
+            st.write(f"For more search result please visit: https://www.ncbi.nlm.nih.gov/search/all/?term={keyword}")
+            
+            st.subheader("OpenAI summarization")
 
-    else:
-        st.write("No articles found for the given keyword.")
+            # if st.button('Summarize'):
+            if not openai_api_key.startswith('sk-'):
+                st.warning('Please enter your own OpenAI API key!', icon='⚠')
+            else:
+                # overview = f"The DataFrame has {article_df.shape[0]} rows and {article_df.shape[1]} columns. The columns are: {', '.join(article_df.columns)}."
+                overview = "you are an AI assistant on a spaceship helping human to understand the info of research on a dataset. the main mission is helping the human to learn about the gene and potential health related function or symptom the gene of interest would cause. make sure to greet the commander with some cheerful opening. The following stream of information are the latest 7 publication related to the keyword {keyword} from PubMed."
+                content = article_df.to_string()
+                prompt_text = f"{overview}\n\nhere are all the info in a dataframe: {content}\n\nBased on the above data, can you summarize them into a 150 words report for the astronaut commander in a nutrigenomics one stop shop?"
 
-st.header("Individual Astronaut's SNPs")
+                generate_response(prompt_text)
+                
 
+                if show_prompt:
+                    # st.success('Prompt generated successfully!')
+                    st.info(f"Prompt used: \n\n{prompt_text}")
+                
+                    
+
+        else:
+            st.write("No articles found for the given keyword.")
+
+st.header("Stellar SNPeek")
+st.write("To check all the SNPs your astronauts have and compare with known pathogenic or disease-associated SNPs")
 uploaded_file = st.file_uploader("Upload the VCF here")
 
 # a button for example data
 if st.button("Use example data"):
-    uploaded_file = "./data/Astro1_MTHFR.vcf"
+    uploaded_file = "./data/chr01/Astro1_MTHFR.vcf"
 
 if uploaded_file is not None:
 
@@ -144,9 +156,19 @@ if uploaded_file is not None:
     dataframe = dataframe[["position", "reference", "alternative"]]
     st.write(dataframe)
 
-st.header('Interactive Scatterplot of Nutritional Rank vs Dehydrated Weight')
+    st.subheader("Known pathogenic or disease-associated SNPs.")
+    Astr_SNPS_df = pd.read_csv("data/Astr_SNPS.csv")
+    Astr_SNPS_df.columns = ["position", "Most Severe clinical significance", "condition"]
+    st.write(Astr_SNPS_df)
 
 
+st.header('FuelFolio Finder')
+
+st.write("to find out the best food for your astronauts but also save the most fuel")
+
+human_daily_nutrition_df = pd.read_csv("data/human_daily_nutrition.csv")
+st.subheader("human daily nutrition")
+st.dataframe(human_daily_nutrition_df)
 # mock nutrition and dehydration data
 df = pd.DataFrame({
     "Nutritional Rank": np.random.randint(1, 100, 50), # Random ranks between 1 and 100
@@ -161,3 +183,13 @@ fig = px.scatter(df, x="Nutritional Rank", y="Dehydrated Weight", title="Nutriti
                  template="plotly_white")
 
 st.plotly_chart(fig, use_container_width=True)
+
+st.write("for seed weight please search here: https://ser-sid.org/")
+
+# embed streamlit docs in a streamlit app
+components.iframe("https://ser-sid.org/", width=1024, height=640, scrolling=True)
+
+# footer
+st.divider()
+"Embark on a Journey of Taste and Health — Where Every Bite is an Adventure. © 2024. Dive deeper, pack smarter, and elevate your meals with Chromosome Culinary Crew. Bon Voyage on your culinary exploration!"
+
